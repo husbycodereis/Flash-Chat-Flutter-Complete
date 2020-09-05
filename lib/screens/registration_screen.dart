85b60a959f89c90285becce6e843c646bc0f9f14
Ali Riza Reisoglu <alirizareisoglu@gmail.com>
2020-09-05 16:52:37 +0300

    Update registration_screen.dart

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,21 +1,20 @@
+import 'package:flash_chat/constants/constants.dart';
+import 'package:flash_chat/constants/routes.dart';
+import 'package:flash_chat/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/components/rounded_button.dart';
-import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'chat_screen.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
-  static const String id = 'registration_screen';
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  final _auth = FirebaseAuth.instance;
   bool showSpinner = false;
   String email;
   String password;
+  final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +29,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Flexible(
-                child: Hero(
+                              child: Hero(
                   tag: 'logo',
                   child: Container(
                     height: 200.0,
@@ -42,43 +41,40 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 height: 48.0,
               ),
               TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration:
-                    kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-              ),
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFileDecoration.copyWith(
+                      hintText: "Enter your e-mail")),
               SizedBox(
                 height: 8.0,
               ),
               TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password'),
-              ),
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFileDecoration.copyWith(
+                      hintText: "Enter your password")),
               SizedBox(
                 height: 24.0,
               ),
               RoundedButton(
-                title: 'Register',
-                colour: Colors.blueAccent,
+                color: Colors.blueAccent,
+                text: "Register",
                 onPressed: () async {
                   setState(() {
                     showSpinner = true;
                   });
                   try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
+                    final user = await _auth.createUserWithEmailAndPassword(
                         email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, ChatScreen.id);
+                    if (user != null) {
+                      Navigator.pushNamed(context, Routes.chatScreen);
                     }
-
                     setState(() {
                       showSpinner = false;
                     });
